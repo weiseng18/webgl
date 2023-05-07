2996193ce5defcd94576d289858233d1f03e4bcf
weiseng18 <tws.hci@gmail.com>
2023-05-07 23:14:57 +0800

    somewhat working circle collision

--- structs.js
+++ structs.js
@@ -6,6 +6,29 @@ class Circle {
         this.radius = radius;
         this.speed = speed;
     }
+
+    distance(other) {
+        return Math.sqrt(Math.pow(this.pos.x - other.pos.x, 2) + Math.pow(this.pos.y - other.pos.y, 2));
+    }
+
+    checkIntersection(other) {
+        const dist = this.distance(other);
+
+        // Handle degenerate cases
+        if (dist == 0) {
+            const prev_radius = this.radius - this.speed;
+            const prev_other_radius = other.radius - other.speed;
+            return this.radius == other.radius ||
+                (prev_radius < prev_other_radius && this.radius > other.radius) ||
+                (prev_radius > prev_other_radius && this.radius < other.radius);
+        }
+
+        if (dist <= this.radius - other.radius)
+            return false; // other is inside this
+        if (dist <= other.radius - this.radius)
+            return false; // this is inside other
+        return dist <= this.radius + other.radius;
+    }
 }
 
 // Collection of circles
@@ -23,26 +46,54 @@ class CircleArray {
             g: Math.random(),
             b: Math.random()
         };
-        const speed = Math.random() * 3;
+        const speed = Math.random() * 0.5 + 1;
         this.circles.push(new Circle(pos, color, radius, speed));
     }
 
     tick() {
-        // Each circle should grow and once it hits the wall, it should shrink.
-        // Then, once it hits a certain size, it should start growing again.
+        // Keep track of which circles have already collided
+        let hasCollision = [];
         for (let i=0; i<this.circles.length; i++) {
-            const cur = this.circles[i];
-            cur.radius += cur.speed;
+            hasCollision.push(false);
+        }
 
-            // If circle hits the wall, start shrinking
-            if (cur.pos.x + cur.radius > gl.canvas.width || cur.pos.x - cur.radius < 0 ||
-                cur.pos.y + cur.radius > gl.canvas.height || cur.pos.y - cur.radius < 0) {
-                cur.speed *= -1;
+        // Create copy and increase radius by the speed
+        let cpy = this.circles.slice();
+        for (let i=0; i<cpy.length; i++) {
+            cpy[i].radius += cpy[i].speed;
+        }
+
+        for (let i=0; i<cpy.length; i++) {
+            let c1 = cpy[i];
+
+            // check for collision with edge of canvas
+            if (c1.pos.x - c1.radius < 0 || c1.pos.x + c1.radius > gl.canvas.width ||
+                c1.pos.y - c1.radius < 0 || c1.pos.y + c1.radius > gl.canvas.height) {
+                hasCollision[i] = true;
+                continue;
             }
+            
+            // check for collision with other circles
+            for (let j=i+1; j<cpy.length; j++) {
+                let c2 = cpy[j];
+                if (c1.checkIntersection(c2)) {
+                    hasCollision[i] = true;
+                    hasCollision[j] = true;
+                }
+            }
+        }
 
-            // If circle degenerates to a point, start growing again
-            if (cur.radius <= 0) {
-                cur.speed *= -1;
+        // Update circles
+        for (let i=0; i<this.circles.length; i++) {
+            // check for collision, then update radius
+            if (hasCollision[i])
+                this.circles[i].speed *= -1;
+            this.circles[i].radius += this.circles[i].speed;
+
+            // handle degenerate case
+            if (this.circles[i].radius < 0) {
+                this.circles[i].radius = 0;
+                this.circles[i].speed *= -1;
             }
         }
     }
