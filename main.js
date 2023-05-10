7a9d42be3be7de4128822d6e8da00a2e95e86714
weiseng18 <tws.hci@gmail.com>
2023-05-10 20:50:27 +0800

    increase thickness

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const shaders = {
     attribute vec2 position;
     void main() {
         gl_Position = vec4(position, 0, 1.0);
-        gl_PointSize = 30.0;
+        gl_PointSize = 50.0;
     }
     `,
     fragment: `
